f4abca6e74ce7ef8083e5b5967a1cc4f7996671a
ondicho <ondicho53@gmail.com>
2022-03-07 14:51:31 +0300

    creates a form to add posts in posts component

--- src/Posts.tsx
+++ src/Posts.tsx
@@ -1,10 +1,56 @@
+import {
+  Button,
+  Card,
+  CardText,
+  CardTitle,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
 import Navigationbar from "./Navbar";
 
 const Posts = () => {
   return (
     <>
       <Navigationbar />
-     
+      <br />
+      <Container>
+        <Row>
+          <Col sm={8}></Col>
+          <Col sm={4}>
+            <Card body>
+              <CardTitle>Create Post</CardTitle>
+              <CardText>
+                <Form>
+                  <FormGroup>
+                    <Label for="exampleEmail">Title</Label>
+                    <Input />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="exampleSelect">Category</Label>
+                    <Input type="select" name="select" id="exampleSelect">
+                      <option>Motivation</option>
+                      <option>Art</option>
+                      <option>Technology</option>
+                      <option>Finance</option>
+                    </Input>
+                  </FormGroup>
+
+                  <FormGroup>
+                    <Label for="exampleText">Description</Label>
+                    <Input type="textarea" name="text" id="exampleText" />
+                  </FormGroup>
+                  <Button>Submit</Button>
+                </Form>
+              </CardText>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
